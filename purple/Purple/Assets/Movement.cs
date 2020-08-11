9b0c85d31612b0393351213cbcc172b91bc1aaa2
kaneda <samflynn19121@gmail.com>
2020-08-12 00:15:22 +0200

    testing stuff
    nothing important

---
+++ purple/Purple/Assets/Movement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+   public float speed;
+   public float turnSpeed;
+   public float horizontalInput;
+   public float verticalInput;
+
+ 
+    void Start()
+    { 
+
+;
+    }
+
+    
+    void Update()
+    { 
+ verticalInput = Input.GetAxis("Vertical");
+horizontalInput = Input.GetAxis ("Horizontal");
+transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}
